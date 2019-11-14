922c660d694f24795df93b14a6464863c72574e9
Drew FINNIS <dfinnis@e2r1p1.42.fr>
2019-11-14 12:00:26 +0100

    refacto/draw functions ordered

--- src/drawnewgame.go
+++ src/drawnewgame.go
@@ -26,16 +26,16 @@ func drawSelectHuman(screen *ebiten.Image, g *game, shift float64) {
 	screen.DrawImage(imgSelect, opSelect)
 }
 
-func drawSelectAI(screen *ebiten.Image, g *game, shift float64) {
+func drawSelectHotseat(screen *ebiten.Image, g *game, shift float64) {
 	opSelect := &ebiten.DrawImageOptions{}
-	opSelect.GeoM.Translate(90/scaleSelect+shift, 698/scaleSelect)
+	opSelect.GeoM.Translate(90/scaleSelect+shift, 495/scaleSelect)
 	opSelect.GeoM.Scale(scaleSelect, scaleSelect)
 	screen.DrawImage(imgSelect, opSelect)
 }
 
-func drawSelectHotseat(screen *ebiten.Image, g *game, shift float64) {
+func drawSelectAI(screen *ebiten.Image, g *game, shift float64) {
 	opSelect := &ebiten.DrawImageOptions{}
-	opSelect.GeoM.Translate(90/scaleSelect+shift, 495/scaleSelect)
+	opSelect.GeoM.Translate(90/scaleSelect+shift, 698/scaleSelect)
 	opSelect.GeoM.Scale(scaleSelect, scaleSelect)
 	screen.DrawImage(imgSelect, opSelect)
 }
@@ -48,8 +48,8 @@ func drawSelectPlayer(screen *ebiten.Image, g *game, player bool) {
 		shift = float64(column1) / scaleSelect
 	}
 	if p.hotseat == true {
-		drawSelectHotseat(screen, g, shift)
 		drawSelectHuman(screen, g, shift)
+		drawSelectHotseat(screen, g, shift)
 		drawSelectAI(screen, g, shift)
 	} else if p.aiPlayer == false {
 		drawSelectHuman(screen, g, shift)
@@ -83,6 +83,16 @@ func drawSelect(screen *ebiten.Image, g *game) {
 	}
 }
 
+func drawHuman(screen *ebiten.Image, g *game) {
+	text.Draw(screen, `Human`, mplusBigFont, newGameColumnBlack, row*4, color.Black)
+	text.Draw(screen, `Human`, mplusBigFont, newGameColumnBlack+column1, row*4, color.White)
+}
+
+func drawHotseat(screen *ebiten.Image, g *game) {
+	text.Draw(screen, `Hotseat`, mplusBigFont, newGameColumnBlack, row*6, color.Black)
+	text.Draw(screen, `Hotseat`, mplusBigFont, newGameColumnBlack+column1, row*6, color.White)
+}
+
 func drawAIplayer(screen *ebiten.Image, g *game, depth uint8, column int, color color.Color) {
 	text.Draw(screen, `AI - depth`, mplusBigFont, newGameColumnBlack+column, row*8, color)
 	text.Draw(screen, strconv.Itoa(int(depth)), mplusBigFont, newGameColumnBlack+column+400, row*8, color)
@@ -93,16 +103,6 @@ func drawAIplayer(screen *ebiten.Image, g *game, depth uint8, column int, color 
 func drawAI(screen *ebiten.Image, g *game) {
 	drawAIplayer(screen, g, g.ai0.depth, 0, color.Black)
 	drawAIplayer(screen, g, g.ai1.depth, column1, color.White)
-}
-
-func drawHuman(screen *ebiten.Image, g *game) {
-	text.Draw(screen, `Human`, mplusBigFont, newGameColumnBlack, row*4, color.Black)
-	text.Draw(screen, `Human`, mplusBigFont, newGameColumnBlack+column1, row*4, color.White)
-}
-
-func drawHotseat(screen *ebiten.Image, g *game) {
-	text.Draw(screen, `Hotseat`, mplusBigFont, newGameColumnBlack, row*6, color.Black)
-	text.Draw(screen, `Hotseat`, mplusBigFont, newGameColumnBlack+column1, row*6, color.White)
 }
 
 func drawHighlight(screen *ebiten.Image, g *game) {
