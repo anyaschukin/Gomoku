f2b8d81a6183828c986fba0f0dd6c3a5c601a31f
Anya SCHUKIN <anya.schukin@gmail.com>
2019-11-17 13:33:32 +0100

    cleanup old files

--- AI/old_main.go
+++ AI/old_main.go
@@ -8,33 +8,39 @@ package ai
 // 	play "Gomoku/play"
 // )
 
-// func GameLoop(G *play.Game) {
-// 	validated := false
-// 	coordinate := play.RandomCoordinate() /////
-// 	for i := 0; i < 10000; i++ {     //moves
-// 		validated, coordinate = play.PlaceRandomIfValid(G)
-// 		if validated == true {
-// 			play.Capture(coordinate, G)
-// 			play.DumpGoban(&G.Goban)   //////
-// 			play.CountStones(&G.Goban) /////////
-// 			play.CheckWin(coordinate, G)
-// 			G.Player = play.SwapPlayers(G.Player)
-// 		}
-// 	}
-// 	// update game.moves ++
-// 	//	return err
-// }
+//  create tree
+//  generate board/node
+//  assign value to board
+//  plug board into minimax
+//  choose board
 
-// func main() {
-// 	fmt.Println("Hello world!") ////////
-// 	G := play.InitializeGame()
-// 	// gui.RunEbiten()
-// 	GameLoop(G)
-// 	fmt.Println("Goodbye world!") ////////
-// }
+func GameLoop(G *play.Game) {
+	validated := false
+	coordinate := play.RandomCoordinate() /////
+	for i := 0; i < 10000; i++ {          //moves
+		validated, coordinate = play.PlaceRandomIfValid(G)
+		if validated == true {
+			play.Capture(coordinate, G)
+			play.DumpGoban(&G.Goban)   //////
+			play.CountStones(&G.Goban) /////////
+			play.CheckWin(coordinate, G)
+			G.Player = play.SwapPlayers(G.Player)
+		}
+	}
+	// update game.moves ++
+	//	return err
+}
+
+func main() {
+	fmt.Println("Hello world!") ////////
+	G := play.newGame()
+	// gui.RunEbiten()
+	GameLoop(G)
+	fmt.Println("Goodbye world!") ////////
+}
 
 // func GameLoop(g) {
-// if AI:
+// if AI:git ad
 //		suggest move
 // if human or hotseat:
 //		listen for mouse click 						//###### do first
