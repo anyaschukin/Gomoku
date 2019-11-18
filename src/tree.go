494a465fecd98e5153ca6d43bcd14ae33d16e9a7
Anya SCHUKIN <anya.schukin@gmail.com>
2019-11-18 10:48:37 +0100

    fix recursive ID in tree building

--- src/tree.go
+++ src/tree.go
@@ -11,11 +11,13 @@ import (
 const maxInt = int(^uint(0) >> 1)
 const minInt = -maxInt - 1
 
+var identity int
+
 type node struct {
-	id		int
-	value   int
-	goban   [19][19]position 
-	player	bool
+	id     int
+	value  int
+	goban  [19][19]position
+	player bool
 	// game     *play.Game
 	children []*node
 	bestMove *node
@@ -25,9 +27,9 @@ type node struct {
 
 func newNode(id int, value int, newGoban *[19][19]position, newPlayer bool) *node {
 	return &node{
-		id:   	id,
-		value:	value,	// change this to initialize to zero
-		goban:	*newGoban,
+		id:     id,
+		value:  value, // change this to initialize to zero
+		goban:  *newGoban,
 		player: newPlayer,
 	}
 	// node.board = &play.Game
@@ -52,7 +54,7 @@ func addChild(node *node, parentID int, child *node) int {
 func generateBoardsDepth(depth int8, current *node, id int, player bool) {
 	var y int8
 	var x int8
-	
+
 	if depth > 2 {
 		return
 	}
@@ -60,13 +62,13 @@ func generateBoardsDepth(depth int8, current *node, id int, player bool) {
 		for x = 0; x < 19; x++ {
 			coordinate := coordinate{y, x}
 			// fmt.Printf("depth %d\n", depth)
-			if isMoveValid2(coordinate, &current.goban, player) == true {		// duplicate of isMoveValid w/o *game
+			if isMoveValid2(coordinate, &current.goban, player) == true { // duplicate of isMoveValid w/o *game
 				// id not incrementing correctly in recursive function
-				id += 1
+				identity++
 				newGoban := current.goban
 				placeStone(coordinate, player, &newGoban)
 				value := valueBoard(&newGoban, player)
-				child := newNode(id, value, &newGoban, player)
+				child := newNode(identity, value, &newGoban, player)
 				addChild(current, current.id, child) //
 				generateBoardsDepth(depth+1, child, child.id, !player)
 			}
@@ -76,8 +78,7 @@ func generateBoardsDepth(depth int8, current *node, id int, player bool) {
 }
 
 func createTree(g *game) *node {
-	id := 1
-	root := newNode(id, 10, &g.goban, g.player)
+	root := newNode(0, 10, &g.goban, g.player)
 	generateBoardsDepth(1, root, root.id, root.player)
 	return root
 }
@@ -111,26 +112,23 @@ func minimaxTree(g *game) {
 	// minimaxRecursive(root, 3, alpha, beta, false)	// for some reason, maximizingplayer has to be set to 'false' for this to work
 	// current := root
 	// for current.bestMove != nil {
-		// fmt.Println(current.id)
-		// current = current.bestMove
+	// fmt.Println(current.id)
+	// current = current.bestMove
 	// }
 	// fmt.Println(current.id)
 }
-
 
 //  creates a tree, whose root is the goban
 //  creates all possible moves/boards to depth _, calculates values, add to tree
 //  applies minimax to tree, finds best move
 
-
-
-	// addChild(root, 1, &node{id: 2, Value: 20})
-	// addChild(root, 1, &node{id: 3, Value: 30})
-	// addChild(root, 1, &node{id: 4, Value: 40})
-	// addChild(root, 2, &node{id: 5, Value: 50})
-	// addChild(root, 2, &node{id: 6, Value: 60})
-	// addChild(root, 2, &node{id: 7, Value: 70})
-	// addChild(root, 3, &node{id: 8, Value: 80})
-	// addChild(root, 3, &node{id: 9, Value: 90})
-	// addChild(root, 3, &node{id: 10, Value: 100})
-	// addChild(root, 4, &node{id: 11, Value: 110})
+// addChild(root, 1, &node{id: 2, Value: 20})
+// addChild(root, 1, &node{id: 3, Value: 30})
+// addChild(root, 1, &node{id: 4, Value: 40})
+// addChild(root, 2, &node{id: 5, Value: 50})
+// addChild(root, 2, &node{id: 6, Value: 60})
+// addChild(root, 2, &node{id: 7, Value: 70})
+// addChild(root, 3, &node{id: 8, Value: 80})
+// addChild(root, 3, &node{id: 9, Value: 90})
+// addChild(root, 3, &node{id: 10, Value: 100})
+// addChild(root, 4, &node{id: 11, Value: 110})
