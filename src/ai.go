8f39a4f28f758c299a070257c3d57bdc6a24a533
Drew FINNIS <dfinnis@e2r1p5.42.fr>
2019-11-19 08:55:42 +0100

    feature/opening moves

--- src/ai.go
+++ src/ai.go
@@ -42,10 +42,33 @@ func isPlayerHotseat(g *game) bool {
 	return false
 }
 
+func openingMoves(g *game) {
+	start := time.Now()
+	openingBlack := coordinate{9, 9}
+	if g.move == 0 { // Black opening
+		elapsed := (time.Since(start))
+		g.ai0.suggest = openingBlack
+		g.ai0.timer = elapsed
+	}
+	if g.move == 1 { // White opening
+		openingWhite := openingBlack
+		if positionOccupied(openingBlack, &g.goban) == true {
+			openingWhite = coordinate{8, 8}
+		}
+		elapsed := (time.Since(start))
+		g.ai1.suggest = openingWhite
+		g.ai1.timer = elapsed
+	}
+}
+
 // aiSuggestMove, if player is AI call AI to suggest a move
 func aiSuggestMove(g *game) {
-	if isPlayerHuman(g) == false || isPlayerHotseat(g) == true {
+	if isPlayerHuman(g) == false || isPlayerHotseat(g) == true { /////do we need this??!!!
 		// artificialIdiot(g)
+		if g.move < 2 {
+			openingMoves(g)
+			return
+		}
 		minimaxTree(g)
 	}
 }
