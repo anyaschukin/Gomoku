56cafc5229c6572ce2f6a558b9236a1a56740bd6
Anya Schukin <anya.schukin@gmail.com>
2020-01-07 11:18:54 +0100

    refacto/lots of print statements to explore tree and best route

--- src/aiMinimax.go
+++ src/aiMinimax.go
@@ -1,5 +1,7 @@
 package gomoku
 
+import "fmt"
+
 // //  Alpha is the best choice which has been found so far for the maximising player.
 // //  Beta is the best choice which has been found so far for the minimising player
 
@@ -25,13 +27,41 @@ func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPl
 
 	node.maximizingPlayer = maximizingPlayer
 	generateBoards(node, node.coordinate, node.lastMove)
+	for i := range node.children {
+		child := node.children[i]
+		fmt.Printf("child.id = %d, child.value = %d\n", child.id, child.value) //////
+	}
+	fmt.Printf("\n") //////
+	// fmt.Printf("node id: %v\n", node.id)
+	// fmt.Printf("node value: %v\n", node.value)
+	// fmt.Printf("node coordinate: %v\n", node.coordinate)
+	// fmt.Printf("node id: %v\n", node.id)
+	// fmt.Printf("node id: %v\n", node.id)
+	// fmt.Printf("node id: %v\n", node.id)
+	// fmt.Printf("node id: %v\n", node.id)
+	// fmt.Printf("node id: %v\n", node.id)
+	// fmt.Printf("node id: %v\n", node.id)
+	// id               int
+	// value            int
+	// goban            [19][19]position
+	// coordinate       coordinate
+	// lastMove         coordinate
+	// player           bool // black or white
+	// maximizingPlayer bool // used by miniMax algo
+	// children         []*node
+	// bestMove         *node
+
 	if maximizingPlayer == true {
 		maxValue := alpha // set maxEval to -infinity
 		for idx := range node.children {
 			child := node.children[idx]
 			value := minimaxRecursive(child, depth-1, alpha, beta, false)
+			// if len(node.children) == 0 {
+			// 	value = node.value
+			// }
 			maxValue = max(maxValue, value)
 			alpha = max(alpha, value)
+			// if node.bestMove == nil || child.value == maxValue {
 			if node.bestMove == nil || child.value > node.bestMove.value {
 				node.bestMove = child
 			}
@@ -45,8 +75,10 @@ func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPl
 		for idx := range node.children {
 			child := node.children[idx]
 			value := minimaxRecursive(child, depth-1, alpha, beta, true)
+
 			minValue = min(minValue, value)
 			beta = min(beta, value)
+			// if node.bestMove == nil || child.value == minValue {
 			if node.bestMove == nil || child.value < node.bestMove.value {
 				node.bestMove = child
 			}
