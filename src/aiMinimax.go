6f0fbc314dd580070b527f9153f3d4f7a97cceb1
Anne SCHUKIN <anya.schukin@gmail.com>
2020-01-20 12:54:13 +0100

    refacto/remove minimax debugging print statements

--- src/aiMinimax.go
+++ src/aiMinimax.go
@@ -19,7 +19,7 @@ func min(a, b int) int {
 	return b
 }
 
-func minimaxRecursive(node *node, depth uint8, startDepth uint8, alpha int, beta int, maximizingPlayer bool) int {
+func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPlayer bool) int {
 
 	if depth == 0 {
 		return node.value
@@ -27,35 +27,13 @@ func minimaxRecursive(node *node, depth uint8, startDepth uint8, alpha int, beta
 
 	generateBoards(node, node.coordinate, node.lastMove)
 
-	// fmt.Printf("parent.id = %d, parent.player = %v, parent.maximingPlayer = %v, parent.coordinate: %v, parent.value = %d\n", node.id, node.player, node.maximizingPlayer, node.coordinate, node.value) //////
-	// // if node.id == 357130 {
-	// // 	dumpGobanBlank(&node.goban)
-	// // }
-	// for i := range node.children {
-	// 	child := node.children[i]
-	// 	fmt.Printf("child.id = %d, child.player = %v, child.maximizingPlayer: %v, child.coordinate: %v, child.value = %d\n", child.id, child.player, child.maximizingPlayer, child.coordinate, child.value) //////
-	// 	// if child.value >= 2039951306 {
-	// 	// 	fmt.Printf("Oh Hi\n") //////
-	// 	// }
-	// 	// if child.id == 361550 {
-	// 	// 	dumpGobanBlank(&node.goban)
-	// 	// }
-	// }
-	// fmt.Printf("\n") //////
-
 	if maximizingPlayer == true {
 		maxValue := minInt // set maxEval to -infinity
 		for idx := range node.children {
 			child := node.children[idx]
-			value := minimaxRecursive(child, depth-1, startDepth, alpha, beta, false)
-			// fmt.Printf("id = %d, player = %v, maximizingPlayer: %v, coordinate: %v, value = %d\n", child.id, child.player, child.maximizingPlayer, child.coordinate, child.value) //////
-			// fmt.Printf("maxValue: %v, value: %v\n", maxValue, value)
+			value := minimaxRecursive(child, depth-1, alpha, beta, false)
 			maxValue = max(maxValue, value)
 			alpha = max(alpha, value)
-			// if depth == startDepth && (cooronate not set or child.value > best move value) {
-				// best move coordinate = child.coordinate
-			//}
-			// if node.bestMove == nil || child.value > node.bestMove.value {
 			if node.bestMove == nil || value == maxValue {
 				node.bestMove = child
 			}
@@ -63,22 +41,14 @@ func minimaxRecursive(node *node, depth uint8, startDepth uint8, alpha int, beta
 				break
 			}
 		}
-		// if depth == startDepth {
-		// 	return node.bestMove.id /// best move coordinate
-		// } else {
-			// fmt.Printf("maxValue: %v\n\n", maxValue)//////////
-			return maxValue
-		// }
+		return maxValue
 	} else {
 		minValue := maxInt // set maxEval to +infinity
 		for idx := range node.children {
 			child := node.children[idx]
-			value := minimaxRecursive(child, depth-1, startDepth, alpha, beta, true)
-			// fmt.Printf("id = %d, player = %v, maximizingPlayer: %v, coordinate: %v, value = %d\n", child.id, child.player, child.maximizingPlayer, child.coordinate, child.value) //////
-			// fmt.Printf("minValue: %v, value: %v\n", minValue, value)
+			value := minimaxRecursive(child, depth-1, alpha, beta, true)
 			minValue = min(minValue, value)
 			beta = min(beta, value)
-			// if node.bestMove == nil || child.value < node.bestMove.value {
 			if node.bestMove == nil || value == minValue {
 				node.bestMove = child
 			}
@@ -86,7 +56,6 @@ func minimaxRecursive(node *node, depth uint8, startDepth uint8, alpha int, beta
 				break
 			}
 		}
-		// fmt.Printf("minValue: %v\n\n", minValue)
 		return minValue
 	}
 }
