a9c6ef2b18e49ed455d0251bd6c513a9be46adfa
Anne SCHUKIN <anya.schukin@gmail.com>
2020-03-02 15:58:11 +0100

    fix/pprof works... can profile CPU and MEM usage

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import (
 	gomoku "Gomoku/src"
-	// "github.com/pkg/profile"
+	// "flag"
+	"github.com/pkg/profile"
+	// "runtime"
+	// "log"
+	// "os"
+	// "runtime/pprof"
 )
 
+// var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
-	// defer profile.Start().Stop()
+	// flag.Parse()
+	// if *cpuprofile != "" {
+	// f, err := os.Create(*cpuprofile)
+	// if err != nil {
+	// log.Fatal(err)
+	// }
+	// pprof.StartCPUProfile(f)
+	// defer pprof.StopCPUProfile()
+	// }
+	// runtime.SetCPUProfileRate(200)
+	// profile.CPUProfileRate(1),
+	// profile.NoShutdownHook
+	defer profile.Start(profile.CPUProfile, profile.ProfilePath(".")).Stop()
 	gomoku.Play()
 }
 
