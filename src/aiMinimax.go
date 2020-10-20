4cfbf2b8f4beeaa64cd65bc212498a42b1cf7ab8
Anya Schukin <anya.schukin@gmail.com>
2020-10-20 16:18:45 +0200

    refacto/initialize value on the fly

--- src/aiMinimax.go
+++ src/aiMinimax.go
@@ -45,14 +45,14 @@ func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPl
 	fmt.Printf("depth = %d, child.id = %d, child.player = %v, child.maximizingPlayer: %v, child.coordinate: %v, child.value = %d\n", depth, child.id, child.player, child.maximizingPlayer, child.coordinate, child.value)
 	}
 
-	var value int
+	// var value int
 	// tmp := newNode(0, 0, &node.goban, node.coordinate, node.lastMove, !node.player, node.maximizingPlayer, node.captures.capture0, node.captures.capture1, node)
 	// best := newNode(0, 0, &node.goban, node.coordinate, node.lastMove, !node.player, node.maximizingPlayer, node.captures.capture0, node.captures.capture1, node)
 	if maximizingPlayer == true {
 		maxValue := minInt // set value to -infinity
 		for idx := range node.children {
 			child := node.children[idx]
-			value = minimaxRecursive(child, depth-1, alpha, beta, false)
+			value := minimaxRecursive(child, depth-1, alpha, beta, false)
 			fmt.Printf("value = %d, maxValue = %d\n", value, maxValue)
 			// maxValue = max(value, maxValue)
 			if value > maxValue {
@@ -74,7 +74,7 @@ func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPl
 		minValue := maxInt // set value to +infinity
 		for idx := range node.children {
 			child := node.children[idx]
-			value = minimaxRecursive(child, depth-1, alpha, beta, true)
+			value := minimaxRecursive(child, depth-1, alpha, beta, true)
 			fmt.Printf("value = %d, minValue = %d\n", value, minValue)
 			// minValue = min(value, minValue)
 			if value < minValue {
