297f2e7a15075a36db39f2803db2d4648fe2fd28
Anya Schukin <anya.schukin@gmail.com>
2020-10-21 17:05:43 +0200

    refacto/remove debug comments

--- src/aiMinimax.go
+++ src/aiMinimax.go
@@ -1,16 +1,7 @@
 package gomoku
 
-// import "fmt"
-
 //  Alpha is the tmp choice which has been found so far for the maximising player.
 //  Beta is the tmp choice which has been found so far for the minimising player
-
-// func max(value int, best *node, tmp_value int, tmp *node) (int, *node) {
-// 	if value > tmp_value {
-// 		return value, best
-// 	}
-// 	return tmp_value, tmp
-// }
 
 func max(a, b int) int {
 	if a > b {
@@ -45,25 +36,15 @@ func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPl
 	// fmt.Printf("depth = %d, child.id = %d, child.player = %v, child.maximizingPlayer: %v, child.coordinate: %v, child.value = %d\n", depth, child.id, child.player, child.maximizingPlayer, child.coordinate, child.value)
 	// }
 
-	// var value int
-	// tmp := newNode(0, 0, &node.goban, node.coordinate, node.lastMove, !node.player, node.maximizingPlayer, node.captures.capture0, node.captures.capture1, node)
-	// best := newNode(0, 0, &node.goban, node.coordinate, node.lastMove, !node.player, node.maximizingPlayer, node.captures.capture0, node.captures.capture1, node)
 	if maximizingPlayer == true {
 		maxValue := minInt // set value to -infinity
 		for idx := range node.children {
 			child := node.children[idx]
 			value := minimaxRecursive(child, depth-1, alpha, beta, false)
-			// fmt.Printf("value = %d, maxValue = %d\n", value, maxValue) //
-			// maxValue = max(value, maxValue)
 			if value > maxValue {
 				node.bestMove = child
-				// best = tmp
 				maxValue = value
 			}
-			// fmt.Printf("new maxValue = %d\n", maxValue) //
-			// if maxValue == tmp.value {
-				// best = tmp
-			// }
 			alpha = max(alpha, maxValue)
 			if alpha >= beta {
 				break
@@ -75,16 +56,10 @@ func minimaxRecursive(node *node, depth uint8, alpha int, beta int, maximizingPl
 		for idx := range node.children {
 			child := node.children[idx]
 			value := minimaxRecursive(child, depth-1, alpha, beta, true)
-			// fmt.Printf("value = %d, minValue = %d\n", value, minValue) //
-			// minValue = min(value, minValue)
 			if value < minValue {
 				node.bestMove = child
 				minValue = value
 			}
-			// fmt.Printf("new minValue = %d\n", minValue) //
-			// if minValue == tmp.value {
-				// best = tmp
-			// }
 			beta = min(beta, minValue)
 			if beta <= alpha {
 				break
