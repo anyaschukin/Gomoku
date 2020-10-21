483869e04fa07b7012d80f32531d71ba9f355f5b
Anya Schukin <anya.schukin@gmail.com>
2020-10-21 17:20:44 +0200

    refacto/small cleanup

--- src/aiPriority.go
+++ src/aiPriority.go
@@ -86,20 +86,35 @@ package gomoku
 // const block2 			= 1
 // const alignFree2 		= 1
 
-const capture10 		= 10000000
-const break5Align 		= 1000000
-const willBeCaptured8 	= -100000
-const align5Win 		= 100000
-const blockWin 			= 12000
-const willBeCaptured2 	= -10000
-const align4Free 		= 10000
-const block3Free 		= 1100
-const capture2 			= 1000
-const align3Free 		= 1000
-const align4FLanked 	= 500
-const align3Flanked 	= 400
-const block2 			= 1
-const align2Free 		= 1
+// const capture10 		= 10000000
+// const break5Align 		= 1000000
+// const willBeCaptured8 	= -100000
+// const align5Win 		= 100000
+// const blockWin 			= 12000
+// const willBeCaptured2 	= -10000
+// const align4Free 		= 10000
+// const block3Free 		= 1100
+// const capture2 			= 1000
+// const align3Free 		= 1000
+// const align4FLanked 	= 500
+// const align3Flanked 	= 400
+// const block2 			= 1
+// const align2Free 		= 1
+
+const capture10         = 1000000
+const break5Align       = 100000
+const willBeCaptured8   = -10000
+const align5Win         = 10000
+const blockWin          = 4200
+const willBeCaptured2   = -2000
+const align4Free        = 1400
+const block3Free        = 1300
+const capture2          = 1000
+const align3Free        = 1000
+const align4FLanked     = 500
+const align3Flanked     = 400
+const block2            = 1
+const align2Free        = 1
 
 // lineInfluence max value =
 // 119182616898071
